c709e179c6615a454bcdd0205649e944de78e4bc
Bölkény Zalán <Kismajesz@gmail.com>
2019-09-27 16:44:54 +0200

    4.feladat

--- Bead/Program.cs
+++ Bead/Program.cs
@@ -18,13 +18,35 @@ namespace GITHUB_beadas
 
 
 
-
+            int kapott = 0;
             Console.WriteLine("Adja meg a lsita meretet: ");
             int listameret=(Convert.ToInt32(Console.ReadLine()));
-            int kapott = 0;
             
 
 
+            try
+            {
+
+
+
+                while (kapott != listameret)
+                {
+                    Console.WriteLine("Adjon meg egy szamot: ");
+                    szamok.Add(Convert.ToInt32(Console.ReadLine()));
+                    kapott++;
+
+                }
+
+
+                Console.WriteLine("A legnagyobb szam: " + szamok.Max());
+                Console.WriteLine("A legkisebb szam: " + szamok.Min());
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Nem szamot adott meg"); ;
+            }
 
             while (kapott != listameret)
             {
