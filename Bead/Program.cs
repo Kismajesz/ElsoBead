cb4281588c5c3523eb8f0d21888574851ca9624e
Bölkény Zalán <Kismajesz@gmail.com>
2019-09-27 16:16:58 +0200

    Add project files.

---
+++ Bead/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITHUB_beadas
+{
+    class Program
+
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            List<int> szamok = new List<int>(10);
+
+
+
+
+
+            int kapott = 0;
+            while (kapott != 10)
+            {
+                Console.WriteLine("Adjon meg egy szamot: ");
+                szamok.Add(Convert.ToInt32(Console.ReadLine()));
+                kapott++;
+
+            }
+
+
+            Console.WriteLine("A legnagyobb szam: " + szamok.Max());
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
