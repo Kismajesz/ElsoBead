f2206088b2be12e9f30b5311be31e7198a01af8b
Bölkény Zalán <Kismajesz@gmail.com>
2019-09-27 16:27:42 +0200

    2.feladat

--- Bead/Program.cs
+++ Bead/Program.cs
@@ -17,11 +17,17 @@ namespace GITHUB_beadas
             List<int> szamok = new List<int>(10);
 
 
-
-
-
             int kapott = 0;
-            while (kapott != 10)
+           
+
+
+            Console.WriteLine("Adja meg a lista nagysagat: ");
+            int listameret = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            while (kapott != listameret)
             {
                 Console.WriteLine("Adjon meg egy szamot: ");
                 szamok.Add(Convert.ToInt32(Console.ReadLine()));
@@ -31,6 +37,7 @@ namespace GITHUB_beadas
 
 
             Console.WriteLine("A legnagyobb szam: " + szamok.Max());
+           
 
 
             Console.ReadKey();
